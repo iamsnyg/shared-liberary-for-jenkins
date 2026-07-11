de7651e816d2e6125be7a70f2236d22a73bc347d
Sunny <sunnygupta6497@gmail.com>
2026-07-12 00:14:46 +0530

    Improve formatting of sonar-scanner command
    Refactor sonar-scanner command for better readability.

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,10 @@
-def call(String SonarQubeAPI, String ProjectName, String ProjectKey){
-    withSonarQubeEnv("${SonarQubeAPI}"){ {
-        sh "$SONAR_HOME/bin/sonar-scanner -Dsonar.projectName=${ProjectName} -Dsonar.projectKey=${ProjectKey} -X"
+def call(String SonarQubeAPI, String ProjectName, String ProjectKey) {
+    withSonarQubeEnv("${SonarQubeAPI}") {
+        sh """
+            \$SONAR_HOME/bin/sonar-scanner \
+            -Dsonar.projectName=${ProjectName} \
+            -Dsonar.projectKey=${ProjectKey} \
+            -X
+        """
     }
 }
